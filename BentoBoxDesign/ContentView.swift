d870ae82a77880713b216b5cafa5cb6144d00088
Josh Yim <jyim@lcs.on.ca>
2023-10-28 18:01:28 -0400

    small changes in bold, adding a few more blocks

--- BentoBoxDesign/ContentView.swift
+++ BentoBoxDesign/ContentView.swift
@@ -31,6 +31,7 @@ struct ContentView: View {
                                 .padding(.bottom,0)
                             Text("ThunderBolt 4")
                                 .font(.system(size: 12))
+                                .bold()
                                 .offset(y:15)
                         }
                     }
@@ -50,6 +51,7 @@ struct ContentView: View {
                                 .bold()
                             Text("Faster CPU")
                                 .font(.system(size: 11))
+                                .bold()
                                 .offset(y:15)
                         }
                     }
@@ -112,35 +114,114 @@ struct ContentView: View {
                                 .padding()
                             Text("Faster Neural Engine")
                                 .font(.system(size: 11))
+                                .bold()
                                 .offset(y:10)
                         }
                     }
                 }
                 //forth row
-                Rectangle()
-                    .fill(lightgray)
-                    .cornerRadius(15)
+                ZStack{
+                    Rectangle()
+                        .fill(lightgray)
+                        .cornerRadius(15)
+                    Text("high-performance")
+                        .font(.system(size: 20))
+                        .offset(y:-10)
+                        .bold()
+                    Text("media engine with ProRes")
+                        .font(.system(size: 20))
+                        .offset(y:10)
+                        .bold()
+                }
             }
             //Second Column
             VStack{
                 HStack{
-                    Rectangle()
-                        .fill(lightgray)
-                        .frame(width:130,height:120)
-                        .cornerRadius(15)
-                    Rectangle()
-                        .fill(lightgray)
-                        .frame(width:210,height:120)
-                        .cornerRadius(15)
+                    ZStack{
+                        Rectangle()
+                            .fill(lightgray)
+                            .frame(width:130,height:120)
+                            .cornerRadius(15)
+                        VStack{
+                            Text("Up To")
+                                .font(.system(size:11))
+                                .offset(y:-15)
+                            Text("30%")
+                                .font(.system(size: 40))
+                                .offset(y:0)
+                                .bold()
+                            Text("Faster GPU")
+                                .font(.system(size: 11))
+                                .bold()
+                                .offset(y:15)
+                        }
+                    }
+                    ZStack{
+                        Rectangle()
+                            .fill(lightgray)
+                            .frame(width:210,height:120)
+                            .cornerRadius(15)
+                        VStack(alignment:.center){
+                            Text("Industry-Leading")
+                                .font(.system(size: 18))
+                                .offset(y:0)
+                            Text("Performance")
+                                .font(.system(size: 18))
+                                .offset(y:0)
+                            Text("Per watt")
+                                .font(.system(size: 18))
+                                .padding(.top,0)
+                                .padding(.bottom,1)
+                        }
+                        
+                    }
                 }
-                Rectangle()
-                    .fill(lightgray)
-                    .frame(width:350,height:287)
-                    .cornerRadius(15)
-                Rectangle()
-                    .fill(lightgray)
-                    .frame(width:350)
-                    .cornerRadius(15)
+                ZStack{
+                    Rectangle()
+                        .fill(lightgray)
+                        .frame(width:350,height:287)
+                        .cornerRadius(15)
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width:220,height: 220)
+                    VStack{
+                        HStack{
+                            Text("")
+                                .foregroundStyle(.white)
+                                .padding()
+                                .font(.system(size: 70))
+                                .offset(x:20,y:20)
+                            Text("M2")
+                                .foregroundStyle(.white)
+                                .padding()
+                                .fontWeight(.medium)
+                                .font(.system(size: 55))
+                                .offset(x:-15,y:25)
+                        }
+                        Text("MAX")
+                            .foregroundStyle(.white)
+                            .font(.system(size: 30))
+                            .fontWeight(.medium)
+                            .padding()
+                            .offset(y:-30)
+                    }
+                    }
+                ZStack{
+                    Rectangle()
+                        .fill(lightgray)
+                        .frame(width:350)
+                        .cornerRadius(15)
+                        .padding(.top,3)
+                    VStack{
+                        Text("Second-generation")
+                            .font(.system(size: 10))
+                            .fontWeight(.medium)
+                            .offset(y:0)
+                        Text("5 nm technology")
+                            .font(.system(size: 30))
+                            .offset(y:0)
+                    }
+                }
         }
             //3rd column
             VStack{
