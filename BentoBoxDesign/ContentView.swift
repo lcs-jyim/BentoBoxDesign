d1ffafb41f5354cda7499b68869d36bff28100db
Josh Yim <jyim@lcs.on.ca>
2023-10-28 12:42:25 -0400

    the whole wireframe here

--- BentoBoxDesign/ContentView.swift
+++ BentoBoxDesign/ContentView.swift
@@ -9,7 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
+        HStack(alignment:.top){
+            //whole structure
+            VStack(alignment:.leading){
+                //first column
+                //first row
+                HStack{
+                    Rectangle()
+                        .fill(.gray)
+                        .frame(height:110)
+                        .cornerRadius(15)
+                        
+                    Rectangle()
+                        .frame(height:110)
+                        .cornerRadius(15)
+                }
+                //second row
+                Rectangle()
+                    .frame(height:170)
+                    .cornerRadius(15)
+                //third row
+                HStack{
+                    Rectangle()
+                        .frame(height:110)
+                        .cornerRadius(15)
+                    Rectangle()
+                        .frame(height:110)
+                        .cornerRadius(15)
+                }
+                //forth row
+                Rectangle()
+                    .cornerRadius(15)
+            }
+            //Second Column
             VStack{
                 HStack{
                     Rectangle()
