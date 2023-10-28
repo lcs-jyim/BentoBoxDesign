c0d67baf083df3e159a9966f8f8166f93d953bcc
Josh Yim <jyim@lcs.on.ca>
2023-10-28 12:41:45 -0400

    added some pictures and finished the wireframe of the diagram

--- BentoBoxDesign/ContentView.swift
+++ BentoBoxDesign/ContentView.swift
@@ -11,18 +11,40 @@ struct ContentView: View {
     var body: some View {
         HStack {
             VStack{
-                Image(systemName: "bolt.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:100)
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Thunderbolt 4")
-                    
-            }
-            Image(systemName:"circle.square")
+                HStack{
+                    Rectangle()
+                        .frame(width:130,height:110)
+                        .cornerRadius(15)
+                    Rectangle()
+                        .frame(width:210,height:110)
+                        .cornerRadius(15)
+                }
+                Rectangle()
+                    .frame(width:350,height:287)
+                    .cornerRadius(15)
+                Rectangle()
+                    .frame(width:350)
+                    .cornerRadius(15)
         }
-        .frame(width:600,height:600)
+            //3rd column
+            VStack{
+                Rectangle()
+                    .frame(height:110)
+                    .cornerRadius(15)
+                HStack{
+                    Rectangle()
+                        .frame(height:200)
+                        .cornerRadius(15)
+                    Rectangle()
+                        .frame(height:200)
+                        .cornerRadius(15)
+                }
+                Rectangle()
+                    .cornerRadius(15)
+                
+            }
+            }
+        .frame(width:880,height:480)
         .padding()
     }
 }
