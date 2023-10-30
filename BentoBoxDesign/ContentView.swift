0c119a4d8c04503e16d9d5f10443e4a42878c4e9
Josh Yim <jyim@lcs.on.ca>
2023-10-30 12:29:24 -0400

    I have inserted some color configurations and changed the color of texts

--- BentoBoxDesign/ContentView.swift
+++ BentoBoxDesign/ContentView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    let lightgray = Color(red: 0.85, green: 0.85, blue: 0.85)
+    let backGroundGray = Color(red:0.85,green:0.85,blue:0.85)
+    let lightGray = Color(
+           hue: 0/360.0,
+           saturation: 0.0,
+           brightness: 0.93
+       )
+       let mediumGray = Color(
+           hue: 0/360.0,
+           saturation: 0.0,
+           brightness: 0.8
+       )
+       let darkGray = Color(
+           hue: 0/360.0,
+           saturation: 0.0,
+           brightness: 0.45
+       )
+       let lightPurple = Color(
+           hue: 272.0/360.0,
+           saturation: 0.3,
+           brightness: 1.0
+       )
+       let mediumPurple = Color(
+           hue: 266.0/360.0,
+           saturation: 0.40,
+           brightness: 0.50
+       )
+       let darkPurple = Color(
+           hue: 296.0/360.0,
+           saturation: 0.55,
+           brightness: 0.60
+    )
     var body: some View {
         HStack(){
             //whole structure
@@ -20,25 +49,24 @@ struct ContentView: View {
                     //first Block
                     ZStack{
                         Rectangle()
-                            .fill(lightgray)
+                            .fill(backGroundGray)
                             .frame(height:120)
                             .cornerRadius(15)
                         VStack{
                             Image("Thunderbolt")
                                 .resizable()
                                 .frame(width:35,height:60)
-                                .offset(x:0,y:0)
                                 .padding(.bottom,0)
                             Text("ThunderBolt 4")
                                 .font(.system(size: 12))
                                 .bold()
-                                .offset(y:15)
+                                .offset(y:13)
                         }
                     }
                     //Second Block
                     ZStack{
                         Rectangle()
-                            .fill(lightgray)
+                            .fill(backGroundGray)
                             .frame(height:120)
                             .cornerRadius(15)
                         VStack{
@@ -46,9 +74,11 @@ struct ContentView: View {
                                 .font(.system(size:11))
                                 .offset(y:-15)
                             Text("20%")
+                                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
                                 .font(.system(size: 40))
-                                .offset(y:0)
-                                .bold()
+                                .fontWeight(.medium)
+                                
+                               
                             Text("Faster CPU")
                                 .font(.system(size: 11))
                                 .bold()
@@ -59,7 +89,7 @@ struct ContentView: View {
                 //second row
                 ZStack{
                     Rectangle()
-                        .fill(lightgray)
+                        .fill(backGroundGray)
                         .frame(height:160)
                         .cornerRadius(15)
                     VStack{
@@ -68,7 +98,9 @@ struct ContentView: View {
                             .offset(y:3)
                         Text("67 billion")
                             .font(.system(size: 45))
-                            .offset(y:0)
+                            
+                            .foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+                            .font(.system(size: 40))
                         Text("Transistors")
                             .font(.system(size: 20))
                             .offset(y:3)
@@ -79,7 +111,7 @@ struct ContentView: View {
                 HStack{
                     ZStack{
                         Rectangle()
-                            .fill(lightgray)
+                            .fill(backGroundGray)
                             .frame(height:120)
                             .cornerRadius(15)
                         VStack{
@@ -90,12 +122,14 @@ struct ContentView: View {
                             Text("Neural")
                                 .font(.system(size: 30))
                                 .bold()
-                                .offset(y:0)
-                                .lineSpacing(0)
+                                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+                                .font(.system(size: 40))
                             Text("Engine")
                                 .font(.system(size: 30))
                                 .bold()
                                 .offset(y:-9)
+                                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+                                .font(.system(size: 40))
                             Text("15.8 trillion ops/s")
                                 .font(.system(size: 11))
                                 .offset(y:2)
@@ -103,33 +137,34 @@ struct ContentView: View {
                     }
                     ZStack{
                         Rectangle()
-                            .fill(lightgray)
+                            .fill(backGroundGray)
                             .frame(height:120)
                             .cornerRadius(15)
                         VStack{
                             Text("40%")
-                                .font(.system(size: 30))
+                                .font(.system(size:40))
                                 .bold()
                                 .offset(y:5)
                                 .padding()
+                                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
                             Text("Faster Neural Engine")
                                 .font(.system(size: 11))
                                 .bold()
-                                .offset(y:10)
+                                .offset(y:5)
                         }
                     }
                 }
                 //forth row
                 ZStack{
                     Rectangle()
-                        .fill(lightgray)
+                        .fill(backGroundGray)
                         .cornerRadius(15)
-                    Text("high-performance")
-                        .font(.system(size: 20))
+                    Text("High-performance")
+                        .font(.system(size: 17))
                         .offset(y:-10)
                         .bold()
                     Text("media engine with ProRes")
-                        .font(.system(size: 20))
+                        .font(.system(size: 17))
                         .offset(y:10)
                         .bold()
                 }
@@ -139,16 +174,16 @@ struct ContentView: View {
                 HStack{
                     ZStack{
                         Rectangle()
-                            .fill(lightgray)
+                            .fill(backGroundGray)
                             .frame(width:130,height:120)
                             .cornerRadius(15)
                         VStack{
                             Text("Up To")
                                 .font(.system(size:11))
                                 .offset(y:-15)
-                            Text("30%")
+                            Text("30%").foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
                                 .font(.system(size: 40))
-                                .offset(y:0)
+                                .font(.system(size: 40))
                                 .bold()
                             Text("Faster GPU")
                                 .font(.system(size: 11))
@@ -158,17 +193,17 @@ struct ContentView: View {
                     }
                     ZStack{
                         Rectangle()
-                            .fill(lightgray)
+                            .fill(backGroundGray)
                             .frame(width:210,height:120)
                             .cornerRadius(15)
                         VStack(alignment:.center){
                             Text("Industry-Leading")
+                                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
                                 .font(.system(size: 18))
-                                .offset(y:0)
                             Text("Performance")
+                                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
                                 .font(.system(size: 18))
-                                .offset(y:0)
-                            Text("Per watt")
+                            Text("Per watt").foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
                                 .font(.system(size: 18))
                                 .padding(.top,0)
                                 .padding(.bottom,1)
@@ -178,7 +213,7 @@ struct ContentView: View {
                 }
                 ZStack{
                     Rectangle()
-                        .fill(lightgray)
+                        .fill(backGroundGray)
                         .frame(width:350,height:287)
                         .cornerRadius(15)
                     Rectangle()
@@ -187,11 +222,12 @@ struct ContentView: View {
                     VStack{
                         HStack{
                             Text("")
+                                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [darkGray,lightGray]), startPoint: .bottomLeading, endPoint: .topTrailing))
                                 .foregroundStyle(.white)
                                 .padding()
                                 .font(.system(size: 70))
                                 .offset(x:20,y:20)
-                            Text("M2")
+                            Text("M2").foregroundStyle(LinearGradient(gradient: Gradient(colors: [mediumGray,lightGray]), startPoint: .leading, endPoint: .trailing))
                                 .foregroundStyle(.white)
                                 .padding()
                                 .fontWeight(.medium)
@@ -199,6 +235,7 @@ struct ContentView: View {
                                 .offset(x:-15,y:25)
                         }
                         Text("MAX")
+                            .foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: .bottomLeading, endPoint: .topTrailing))
                             .foregroundStyle(.white)
                             .font(.system(size: 30))
                             .fontWeight(.medium)
@@ -208,7 +245,7 @@ struct ContentView: View {
                     }
                 ZStack{
                     Rectangle()
-                        .fill(lightgray)
+                        .fill(backGroundGray)
                         .frame(width:350)
                         .cornerRadius(15)
                         .padding(.top,3)
@@ -216,33 +253,89 @@ struct ContentView: View {
                         Text("Second-generation")
                             .font(.system(size: 10))
                             .fontWeight(.medium)
-                            .offset(y:0)
-                        Text("5 nm technology")
+                        Text("5 nm technology").foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
                             .font(.system(size: 30))
-                            .offset(y:0)
+
                     }
                 }
         }
             //3rd column
             VStack{
-                Rectangle()
-                    .fill(lightgray)
-                    .frame(height:120)
-                    .cornerRadius(15)
-                HStack{
+                ZStack{
                     Rectangle()
-                        .fill(lightgray)
-                        .frame(height:240)
+                        .fill(backGroundGray)
+                        .frame(height:120)
                         .cornerRadius(15)
-                    Rectangle()
-                        .fill(lightgray)
-                        .frame(height:240)
-                        .cornerRadius(15)
+                    VStack{
+                        Text("Up To")
+                            .font(.system(size:11))
+                            .offset(y:-15)
+                        Text("96GB")
+                            .foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+                            .font(.system(size: 50))
+                            .bold()
+                        Text("LPDDR5 memory")
+                            .font(.system(size: 11))
+                            .bold()
+                            .offset(y:15)
+                    }
                 }
-                Rectangle()
-                    .fill(lightgray)
-                    .cornerRadius(15)
-                
+                HStack{
+                    ZStack{
+                        Rectangle()
+                            .fill(backGroundGray)
+                            .frame(height:200)
+                            .cornerRadius(15)
+                        VStack{
+                            Image("CPU")
+                                .resizable()
+                                .frame(width: 110,height: 70)
+                            Text("12-core").foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+                                .font(.system(size: 20))
+                                .offset(y:30)
+                                .padding(.top,0)
+                            Text("CPU")
+                                .font(.system(size: 11))
+                                .bold()
+                                .offset(y:35)
+                        }
+                    }
+                    ZStack{
+                        Rectangle()
+                            .fill(backGroundGray)
+                            .frame(height:200)
+                            .cornerRadius(15)
+                        VStack{
+                            Image("GPU")
+                                .resizable()
+                                .frame(width: 50,height: 100)
+                                .offset(y:-10)
+                            Text("Up to")
+                                .font(.system(size: 11))
+                                .offset(y:10)
+                            Text("38-core").foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+                                .font(.system(size: 20))
+                                .offset(y:10)
+                                .padding(.top,0)
+                            Text("GPU")
+                                .font(.system(size: 11))
+                                .bold()
+                                .offset(y:15)
+                        }
+                    }
+                }
+                ZStack{
+                    Rectangle()
+                        .fill(backGroundGray)
+                        .cornerRadius(15)
+                    VStack{
+                        Text("400GB/S").foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+                            .font(.system(size: 40))
+                            .bold()
+                        Text("Memory bandwidth")
+                            .font(.system(size: 20))
+                    }
+                }
             }
             }
         .frame(width:880,height:480)
@@ -253,3 +346,5 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+// .foregroundStyle(LinearGradient(gradient: Gradient(colors: [lightPurple, darkPurple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
